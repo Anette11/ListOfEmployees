58b50e5b62939857c0ac01581a9844b56c1450a7
Anette11 <krivaltseva@gmail.com>
2023-03-14 23:10:00 +0400

    Create composable OneItem

---
+++ app/src/main/java/com/example/listofemployees/ui/components/OneItem.kt
@@ -0,0 +1,88 @@
+package com.example.listofemployees.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.listofemployees.R
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun OneItem(
+    image: Any?,
+    name: String,
+    userTag: String,
+    position: String
+) = Row(
+    modifier = Modifier.fillMaxWidth(),
+    verticalAlignment = Alignment.CenterVertically
+) {
+    GlideImage(
+        modifier = Modifier
+            .size(dimensionResource(id = R.dimen._72dp))
+            .clip(CircleShape),
+        model = image,
+        contentDescription = stringResource(id = R.string.empty)
+    )
+    Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen._16dp)))
+    Column(
+        modifier = Modifier.weight(1f),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = colorResource(id = R.color.black_dark),
+                        fontSize = dimensionResource(id = R.dimen._16sp).value.sp,
+                        fontFamily = FontFamily(Font(R.font.inter_medium))
+                    )
+                ) {
+                    append(name)
+                }
+                append(stringResource(id = R.string.space))
+                withStyle(
+                    style = SpanStyle(
+                        color = colorResource(id = R.color.gray_darker),
+                        fontSize = dimensionResource(id = R.dimen._14sp).value.sp,
+                        fontFamily = FontFamily(Font(R.font.inter_medium))
+                    )
+                ) {
+                    append(userTag)
+                }
+            }
+        )
+        Text(
+            text = position,
+            color = colorResource(id = R.color.gray_darkest),
+            fontSize = dimensionResource(id = R.dimen._13sp).value.sp,
+            fontFamily = FontFamily(Font(R.font.inter_regular))
+        )
+    }
+}
+
+@Composable
+@Preview
+fun OneItemPreview() =
+    OneItem(
+        image = painterResource(id = R.drawable.ic_placeholder),
+        name = stringResource(id = R.string.name),
+        userTag = stringResource(id = R.string.userTag),
+        position = stringResource(id = R.string.position)
+    )
