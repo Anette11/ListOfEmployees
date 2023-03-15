45300c50ae2936f34192a9973db0771ee8bfa7b4
Anette11 <krivaltseva@gmail.com>
2023-03-15 14:21:21 +0400

    Replace AsyncImage with Image

--- app/src/main/java/com/example/listofemployees/ui/components/OneItem.kt
+++ app/src/main/java/com/example/listofemployees/ui/components/OneItem.kt
@@ -1,5 +1,6 @@
 package com.example.listofemployees.ui.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
@@ -7,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -18,7 +20,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.example.listofemployees.R
 
 @Composable
@@ -36,13 +39,20 @@ fun OneItem(
         ),
     verticalAlignment = Alignment.CenterVertically
 ) {
-    AsyncImage(
+    val painter = rememberAsyncImagePainter(
+        ImageRequest.Builder(LocalContext.current)
+            .data(image)
+            .crossfade(true)
+            .placeholder(R.drawable.ic_placeholder)
+            .error(R.drawable.ic_placeholder)
+            .build()
+    )
+    Image(
         modifier = Modifier
             .size(dimensionResource(id = R.dimen._72dp))
             .clip(CircleShape),
-        model = image,
-        contentDescription = stringResource(id = R.string.empty),
-        error = painterResource(id = R.drawable.ic_placeholder)
+        painter = painter,
+        contentDescription = stringResource(id = R.string.empty)
     )
     Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen._16dp)))
     Column(
