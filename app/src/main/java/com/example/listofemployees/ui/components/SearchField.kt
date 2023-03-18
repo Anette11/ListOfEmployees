65f012a53c853bfdf36fbb9d44ec31c6483d6189
Anette11 <krivaltseva@gmail.com>
2023-03-18 15:25:13 +0400

    Set tint for ic_search according to focus state

--- app/src/main/java/com/example/listofemployees/ui/components/SearchField.kt
+++ app/src/main/java/com/example/listofemployees/ui/components/SearchField.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.*
 import androidx.compose.ui.text.TextStyle
@@ -23,67 +26,77 @@ fun SearchField(
     onValueChange: (String) -> Unit,
     onMenuClick: () -> Unit,
     isMenuSelected: Boolean
-) = Row(
-    modifier = Modifier
-        .fillMaxWidth()
-        .padding(
-            start = dimensionResource(id = R.dimen._16dp),
-            end = dimensionResource(id = R.dimen._16dp),
-            top = dimensionResource(id = R.dimen._18dp),
-            bottom = dimensionResource(id = R.dimen._22dp)
-        )
-        .clip(RoundedCornerShape(dimensionResource(id = R.dimen._16dp)))
 ) {
-    TextField(
-        modifier = Modifier.fillMaxWidth(),
-        value = value,
-        onValueChange = onValueChange,
-        placeholder = {
-            Text(
-                text = stringResource(id = R.string.search_field_placeholder),
-                color = colorResource(id = R.color.gray_dark),
+    val focusRequester = remember { FocusRequester() }
+    var isFocused by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                start = dimensionResource(id = R.dimen._16dp),
+                end = dimensionResource(id = R.dimen._16dp),
+                top = dimensionResource(id = R.dimen._18dp),
+                bottom = dimensionResource(id = R.dimen._22dp)
+            )
+            .clip(RoundedCornerShape(dimensionResource(id = R.dimen._16dp)))
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester)
+                .onFocusChanged { focusState -> isFocused = focusState.isFocused },
+            value = value,
+            onValueChange = onValueChange,
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.search_field_placeholder),
+                    color = colorResource(id = R.color.gray_dark),
+                    fontSize = dimensionResource(id = R.dimen._15sp).value.sp,
+                    fontFamily = FontFamily(Font(R.font.inter_medium))
+                )
+            },
+            leadingIcon = {
+                IconButton(
+                    onClick = {},
+                    enabled = false
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_search),
+                        contentDescription = stringResource(id = R.string.search_description),
+                        tint = colorResource(
+                            id = if (isFocused) R.color.black_dark
+                            else R.color.gray_dark
+                        )
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = onMenuClick
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_menu),
+                        contentDescription = stringResource(id = R.string.menu_description),
+                        tint = colorResource(
+                            id = if (isMenuSelected) R.color.purple
+                            else R.color.gray_dark
+                        )
+                    )
+                }
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = colorResource(id = R.color.gray_light),
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            textStyle = TextStyle(
+                color = colorResource(id = R.color.black_dark),
                 fontSize = dimensionResource(id = R.dimen._15sp).value.sp,
                 fontFamily = FontFamily(Font(R.font.inter_medium))
-            )
-        },
-        leadingIcon = {
-            IconButton(
-                onClick = {},
-                enabled = false
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_search),
-                    contentDescription = stringResource(id = R.string.search_description),
-                    tint = colorResource(id = R.color.gray_dark)
-                )
-            }
-        },
-        trailingIcon = {
-            IconButton(
-                onClick = onMenuClick
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_menu),
-                    contentDescription = stringResource(id = R.string.menu_description),
-                    tint = colorResource(
-                        id = if (isMenuSelected) R.color.purple
-                        else R.color.gray_dark
-                    )
-                )
-            }
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = colorResource(id = R.color.gray_light),
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
-        ),
-        textStyle = TextStyle(
-            color = colorResource(id = R.color.black_dark),
-            fontSize = dimensionResource(id = R.dimen._15sp).value.sp,
-            fontFamily = FontFamily(Font(R.font.inter_medium))
-        ),
-        maxLines = integerResource(id = R.integer._1)
-    )
+            ),
+            maxLines = integerResource(id = R.integer._1)
+        )
+    }
 }
 
 @Composable
